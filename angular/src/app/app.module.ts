19dec40692c889e3ff45a13b178bd94c680067be
François Deshayes <francois.deshayes@etudiant.univ-rennes1.fr>
2021-03-23 09:50:29 +0100

    Avancement web

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BouttonComponent } from './resources/boutton/boutton.component';
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    BouttonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/src/app/resources/boutton/boutton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boutton',
+  templateUrl: './boutton.component.html',
+  styleUrls: ['./boutton.component.scss']
+})
+export class BouttonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
